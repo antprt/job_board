ea83d7ca67877f774b23d4c1477ae19ff843c0f1
antprt <antoniopr.apr@gmail.com>
2018-09-26 19:53:32 +0200

    Factories for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :user do
-    email "prueba@prueba.com"
-    password "1234"
+    email { "example@example.com" }
+    password { "1234" }
     
-    trait :user_company do
-      rol "company"
+    trait :company do
+      rol { "company" }
     end
 
-    trait :user_candidate do
-      rol "candidate"
+    trait :candidate do
+      rol { "candidate" }
     end
   end
 end
