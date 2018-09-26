e5fd5771059a15311862ba6a3d43424305f55112
antprt <antoniopr.apr@gmail.com>
2018-09-26 19:51:58 +0200

    application controller integrates methods for render resource and authentication and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  #Render JSON with response for a single object
   def render_resource(resource)
     if resource.errors.empty?
       render json: resource
@@ -9,6 +10,7 @@ class ApplicationController < ActionController::API
     end
   end
 
+  #Format for errors validations
   def validation_error(resource)
     render json: {
       errors: [
@@ -20,6 +22,26 @@ class ApplicationController < ActionController::API
         }
       ]
     }, status: :bad_request
+  end
+
+  #If it isn't user authenticated return 401 or if it isn't brand authenticated return 403
+  def authenticate_brand!
+    authenticate_user!
+    unless current_user.rol == "company"
+      render json: {
+      body: "You haven't permissions."
+    }, status: :forbidden
+    end
+  end
+
+  #If it isn't user authenticated return 401 or if it isn't candidate authenticated return 403
+  def authenticate_candidate!
+    authenticate_user!
+    unless current_user.rol == "candidate"
+      render json: {
+      body: "You haven't permissions."
+    }, status: :forbidden
+    end
   end
 
   protected
