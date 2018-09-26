d4c231abcddd47dc2734e7f6056ae1938c49065f
antprt <antoniopr.apr@gmail.com>
2018-09-26 19:54:06 +0200

    Routes updated for job_adverts

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
                sessions: 'sessions',
                registrations: 'registrations'
              }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :job_adverts, only: [:index, :create]
+
 end
