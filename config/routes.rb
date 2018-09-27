a28ee4c51c6ea4673a49aa6037f661a701f2a17a
antprt <antoniopr.apr@gmail.com>
2018-09-27 19:47:48 +0200

    Update routes. Delete the routes not used of devise gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
-             controllers: {
-               sessions: 'sessions',
-               registrations: 'registrations'
-             }
+  devise_for :users, skip: [:sessions, :registrations]
+  devise_scope :user do
+    post 'login' => 'sessions#create', as: 'user_session'
+    delete 'logout' => 'registrations#destroy', as: 'destroy_user_session'
+    post 'signup' => 'registrations#create', as: 'user_registration'
+  end
 
   resources :job_adverts, only: [:index, :create]
   resources :applies, except: [:destroy]
