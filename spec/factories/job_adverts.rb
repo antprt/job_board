f9785237f60b28adde4f0c4bfbf080ccc414d708
antprt <antoniopr.apr@gmail.com>
2018-09-27 17:36:51 +0200

    Fix in the job_advert creation relation with user

--- spec/factories/job_adverts.rb
+++ spec/factories/job_adverts.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :job_advert do
     title { "Title" }
     description { "description offer" }
-    user { User.where(rol: "company").first || association(:user, :candidate)}
+    user { User.where(rol: "company").first || association(:user, :company)}
   end
 end
