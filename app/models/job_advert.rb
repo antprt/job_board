a65a18315cdf48330ae76340d5f34fa18ad6a848
antprt <antoniopr.apr@gmail.com>
2018-09-27 14:26:27 +0200

    job_advert model update to relations with apply and relations validations with user

--- app/models/job_advert.rb
+++ app/models/job_advert.rb
@@ -1,6 +1,17 @@
 class JobAdvert < ApplicationRecord
+  #RELATIONS AND ITS VALIDATIONS
   belongs_to :user
+  validate :user, :company_present
+  has_many :apply
 
+  #ATTRIBUTES VALIDATIONS
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, allow_blank: true, length: { maximum: 400 }
+
+  #CUSTOM VALIDATIONS
+  def company_present
+    if user.rol != "company"
+      errors.add(:user_id, "El usuario no es una compañía")
+    end
+  end
 end
