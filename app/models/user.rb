3aaecbd64e9786dc4edd1f1d7812f31db22df2f4
antprt <antoniopr.apr@gmail.com>
2018-09-27 14:25:59 +0200

    User model changed to add relation with apply and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #DEVISE CONFIG
   devise :database_authenticatable,
          :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtBlacklist
 
+  #RELATIONS
   has_many :job_advert, dependent: :destroy
+  has_many :apply, dependent: :destroy
 
+  #TYPES TO DEFINE
   ROLES = ["company", "candidate"]
   enum rol: ROLES
 
+  #ATTRIBUTES VALIDATIONS
   validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, length: { maximum: 254 }
   validates_presence_of :rol
+
 end
