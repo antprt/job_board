4e303e94c5a8c761bbd20bb3b1e74f2f4eb8e15a
antprt <antoniopr.apr@gmail.com>
2018-09-27 08:55:24 +0200

    Fix Factory de user. Emails duplicated.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :user do
-    email { "example@example.com" }
     password { "1234" }
     
     trait :company do
+      email { "example@company.com" }
       rol { "company" }
     end
 
     trait :candidate do
+      email { "example@candidate.com" }
       rol { "candidate" }
     end
   end
